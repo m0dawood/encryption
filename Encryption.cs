91d5d01ba6f7f4356debc6322a476a328d4e5f69
Mohamed Dawood <mo.ah.dawood@gmail.com>
2017-09-13 23:52:33 +0200

    hashing algorithm
    this method is for hashing texts may be you can use for hashing password or genrate tokens by the way you it becomes so difecult to detect the origanl string
    
    first we are convert the string to byte array and implement astringbuilder so that generating our strings using it to write our strings
    
    first thing is to add all bytes besides each other so that you cant know which number have to select with other
    
    Suppose you have these three bytes
    [128,15,200]
    the first foreach make them like that
    12815200 now its hard to know which  mix of digits can be the right byte but its not impossible
    now we assign text to the ne string and clear builder
    
    now we will do some thing hard for any to decode  my be impossible but i think this word is not in the programming dctionary
    
    if you not heard before about the math magic square you can google it now that algorithm not uses it but its anti magic square
    immagine you have asquare  that consists of 9 squares 3*3 every one contains number
    our loop is sum every xline and every yline and create new string with this sums
    then add The remaining numbers at the ending
    
    last thing is to decorate your hash by one of Cryptographic hash functions
    you may use md5 if you want short hash

---
+++ Encryption.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mohamed.Dawood
+{
+    public static class Encryption
+    {
+        public static string GenerateHash(string Text)
+        {
+            byte[] tmpSource = ASCIIEncoding.UTF8.GetBytes(Text);
+            StringBuilder byts = new StringBuilder();
+            foreach (var ch in tmpSource)
+            {
+                byts.Append(ch);
+            }
+            Text = byts.ToString();
+            byts.Clear();
+            var sqrt = (int)Math.Sqrt(Text.Length);
+            var sumx = 0;
+            var sumy = 0;
+            for (int i = 0; i < sqrt; i++)
+            {
+                for (int x = 0; x < sqrt; x++)
+                {
+                    sumx += int.Parse(Text[sqrt * i + x].ToString());
+                    sumy += int.Parse(Text[sqrt * x + i].ToString());
+                }
+                byts.Append(sumx);
+                byts.Append(sumy);
+                sumy = 0;
+                sumx = 0;
+            }
+            if (sqrt * sqrt < Text.Length)
+                byts.Append(Text.Substring(sqrt * sqrt));
+
+
+            tmpSource = Encoding.UTF8.GetBytes(byts.ToString());
+            byts.Clear();
+            HashAlgorithm algorithm = SHA256.Create();
+            byte[] tmpHash;
+            tmpHash = algorithm.ComputeHash(tmpSource);
+            for (int i = 0; i < tmpHash.Length; i++)
+            {
+                byts.Append(tmpHash[i].ToString("X2"));
+            }
+            return byts.ToString();
+        }
+    }
+}
